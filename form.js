84830de5e366ae540f657cc59c7a103020dfae2e
Andrew Kobylin <akobylin@snowfall.se>
2019-07-19 18:18:32 +0300

    update form url

--- form.js
+++ form.js
@@ -4,7 +4,12 @@ $(function() {
   $form.on("submit", function(e) {
     e.preventDefault();
 
-    var formUrl = location.protocol + "//" + location.hostname + ":3512/form";
+    var formUrl;
+    if (location.hostname === "localhost") {
+      formUrl = "http://localhost:3512/form";
+    } else {
+      formUrl = "http://46.101.181.63:3512/form";
+    }
 
     $.post(formUrl, {
       name: $form.find('[name="name"]').val(),
