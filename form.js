6bd098e1192b67f339658d9555c74c91cce75909
Andrew Kobylin <akobylin@snowfall.se>
2019-07-21 23:42:12 +0300

    Save user contacts to localStorage. Not show form when do not needed

--- form.js
+++ form.js
@@ -1,34 +1,47 @@
 $(function() {
+  "use strict";
+
+  var USER_LOCAL_STORAGE_KEY = "user";
+
   var $form = $(".sign-up");
+
+  var user = getUser();
+
+  if (!user) {
+    showForm();
+  }
 
   $form.on("submit", function(e) {
     e.preventDefault();
 
-    var formUrl;
-    if (location.hostname === "localhost") {
-      formUrl = "http://localhost:3512/form";
-    } else {
-      formUrl = "http://46.101.181.63:3512/form";
-    }
+    var formUrl = getFormUrl();
 
-    $.post(formUrl, {
+    const userData = {
       name: $form.find('[name="name"]').val(),
       email: $form.find('[name="email"]').val(),
-      phone: $form.find('[name="phone"]').val(),
-      success: function() {
+      phone: $form.find('[name="phone"]').val()
+    };
+
+    $.post(formUrl, userData)
+      .done(function() {
+        saveUser(userData);
         hideForm();
-      }
-    }).fail(function(e) {
-      alert(
-        "There is some problems with storing email. Leave your email to guys near Beetroot stand!"
-      );
-      console.log(e);
-      hideForm();
-    });
+      })
+      .fail(function(e) {
+        alert(
+          "There is some problems with storing email. Leave your email to guys near Beetroot stand!"
+        );
+        console.log(e);
+        hideForm();
+      });
   });
 
   function hideForm() {
     $(".sign-up__container").hide();
+  }
+
+  function showForm() {
+    $(".sign-up__container").show();
   }
 
   $(".sign-up__skip").on("click", function() {
@@ -39,4 +52,31 @@ $(function() {
     "%cHere you can inspect object. But not hacking :)",
     "color: blue; font-size: x-large"
   );
+
+  function getUser() {
+    try {
+      const data = localStorage.getItem(USER_LOCAL_STORAGE_KEY);
+      if (!data) return null;
+
+      return JSON.parse(data);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  function saveUser(user) {
+    try {
+      localStorage.setItem(USER_LOCAL_STORAGE_KEY, JSON.stringify(user));
+    } catch (e) {
+      return null;
+    }
+  }
+
+  function getFormUrl() {
+    if (location.hostname === "localhost") {
+      return "http://localhost:3512/form";
+    } else {
+      return "http://46.101.181.63:3512/form";
+    }
+  }
 });
